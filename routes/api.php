52c4887e2c4bfe09f2b0e7ed5aaed4ec8d25bfdc
Henry Dev <henrydev@Henrys-MacBook-Pro.local>
2021-08-19 18:40:56 +0100

    Refactoring

--- routes/api.php
+++ routes/api.php
@@ -38,7 +38,3 @@ Route::middleware(['auth:sanctum'])->group(function(){
     Route::put('customers/tasks/{id}', [TaskController::class, 'update']);
 
 });
-
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
